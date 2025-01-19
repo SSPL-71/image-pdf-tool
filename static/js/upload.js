b8fdd98c8f4e52999bc9774367b589d22a9c0833
SSPL-71 <ssservicespl@gmail.com>
2025-01-19 16:00:09 +0530

    Updations to PDF Conversions

--- static/js/upload.js
+++ static/js/upload.js
@@ -133,8 +133,8 @@ function convertToPdf() {
             const img = new Image();
             img.src = e.target.result;
             img.onload = function() {
-                const imgWidth = img.width;
-                const imgHeight = img.height;
+                let imgWidth = img.width;
+                let imgHeight = img.height;
                 const aspectRatio = imgWidth / imgHeight;
 
                 if (imgWidth > pageWidth || imgHeight > pageHeight) {
